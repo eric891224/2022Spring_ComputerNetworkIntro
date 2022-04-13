df6ccdbf6b464bdf53382865f3550ad0bfea9ffd
eric891224 <s96006730@gmail.com>
2022-04-13 13:33:22 +0800

    PA6 completed

---
+++ PA6/PA6.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"net"
+	"strconv"
+	"os"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	reader := bufio.NewReader(c)
+	message, errr := reader.ReadString('\n')
+	check(errr)
+	fmt.Printf("Upload file size: %s", message)
+
+	outfile, errf := os.Create("whatever.txt")
+	check(errf)
+	defer outfile.Close()
+	line_count := 1
+	file_len := 0
+	text_len := 0
+	size_limit, _ := strconv.Atoi(message[:len(message)-1])
+	fwriter := bufio.NewWriter(outfile)
+	for {
+		text_line, errt := reader.ReadString('\n')
+		check(errt)
+
+		str_line_count := strconv.Itoa(line_count)
+		_len, errw := fwriter.WriteString(str_line_count + " " + text_line)
+		check(errw)
+
+		file_len += _len
+		line_count += 1
+		text_len += len(text_line)
+		if (text_len >= size_limit) {
+			break
+		}
+	}
+	fwriter.Flush()
+	fmt.Printf("Output file size: %d\n", file_len)
+
+	writer := bufio.NewWriter(c)
+	response := fmt.Sprintf("%d bytes received, %d bytes file generated\n", size_limit, file_len)
+	_, err := writer.WriteString(response)
+	check(err)
+	writer.Flush()
+
+	time.Sleep(5 * time.Second)
+}
+
+func main() {
+	fmt.Println("Launching server...")
+	ln, _ := net.Listen("tcp", ":12007")
+	defer ln.Close()
+
+	for {
+		conn, _ := ln.Accept()
+		defer conn.Close()
+
+		go handleConnection(conn)
+	}
+}
